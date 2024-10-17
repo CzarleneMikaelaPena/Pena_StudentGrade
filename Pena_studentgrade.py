f5430abc5049324fe69fd08a6aa62c18ab04f060
CzarleneMikaelaPena <czarlenemikaelapena04@gmail.com>
2024-10-17 19:41:38 +0800

    Update Pena_studentgrade.py

--- Pena_studentgrade.py
+++ Pena_studentgrade.py
@@ -1,21 +1,6 @@
 Name = input("Enter your name:")
 Section = input("Enter your section:")
 Prelim = float(input("Enter prelim grade:"))
-<<<<<<< HEAD
-Midterm = float(input("Enter midterm grade:"))
-Finals = float(input("Enter final grade:"))
-
-if (100 < Prelim < 40) and (100<Midterm<40) and (100<Finals<40):
-    Prelim_grade = (Prelim*33.33/100)
-    Midterm_grade = (Midterm*33.33/100)
-    Finals_grade = (Finals*33.33/100)
-
-    Final= (Prelim_grade + Midterm_grade + Finals_grade)
-
-    print (Final)
-else:
-    print ("Invalid input. Please input a grade ranging from 40 - 100")
-=======
 
 if (Prelim>=40)and(Prelim<=100):
     Midterm = float(input("Enter midterm grade:"))
